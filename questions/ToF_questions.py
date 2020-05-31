a35c9343ed6bb76103ed929dd1ed95612e7f15c0
Mega-Barrel <saurabhjoshi24business@gmail.com>
2020-05-31 10:10:20 +0530

    Contains True or False questions

---
+++ questions/ToF_questions.py
@@ -0,0 +1,11 @@
+def statements():
+    statement = []
+    statement.append(['True or False: Capital of India is Delhi. ', 'T'])
+    statement.append(['True or False: Capital of France is Madrid. ', 'F'])
+    statement.append(['True or False: The capital of Libya is Benghazi. ', 'F'])
+    statement.append(['True or False: Baby koalas are called joeys. ', 'T'])
+    statement.append(['True or False: The favorite food of the Teenage Mutant Ninja Turtles is pizza. ', 'T'])
+    statement.append(['True or False: The Atlantic Ocean is the biggest ocean on Earth. ', 'F'])
+    return statement
+
+

d2101475b00272fa8a0710090dbb20b255b9c986
Mega-Barrel <saurabhjoshi24business@gmail.com>
2020-05-31 10:09:57 +0530

    Contains Sports Questions.

---
+++ questions/sports_questions.py
@@ -0,0 +1,9 @@
+def statements():
+    statement = []
+    statement.append(['What country won the very first FIFA World Cup in 1930? ', 'Uruguay'])
+    statement.append(['When Michael Jordan played for the Chicago Bulls, how many NBA Championships did he win? ', 'Six'])
+    statement.append(['Which Williams sister has won more Grand Slam titles? ', 'Serena'])
+    statement.append(['Which racer holds the record for the most Grand Prix wins? ', 'Michael Schumacher'])
+    statement.append(['Which boxer was known as “The Greatest” and “The People’s Champion" ', 'Muhammad Ali'])
+    statement.append(['Which Jamaican runner is an 11-time world champion and holds the record in the 100 and 200-meter race? ', 'Usian Bolt'])
+    return statement

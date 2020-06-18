738847302e1d86fb6522a136e76b4b2f8a5e907c
Mega-Barrel <saurabhjoshi24business@gmail.com>
2020-06-18 14:34:05 +0530

    added new geography questions

---
+++ questions/Geography_question.py
@@ -0,0 +1,9 @@
+def geography_question():
+
+    question = []
+    question.append(['Which continent is the largest?', 'Asia'])
+    question.append(['Which of the Seven Wonders is located in Egypt?', 'Pyramid of Giza'])
+    question.append(['What is the capital of New Zealand?', 'Wellington'])
+    question.append(['Which desert is the largest in the world?', 'Sahara Desert'])
+    question.append(['Which city in India would you find the Taj Mahal in?', 'Agra'])
+    return question

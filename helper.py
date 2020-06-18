693afe0dfd7cf0c67d1a2effc899bcdc8042a887
Mega-Barrel <saurabhjoshi24business@gmail.com>
2020-06-18 14:31:31 +0530

    Added new geography questions

--- helper.py
+++ helper.py
@@ -1,13 +1,16 @@
 import random
 
-from questions.sports_questions import statements as s2
-from questions.ToF_questions import statements as s1
+from questions.ToF_questions import ToF as s1
+from questions.sports_questions import sport_ques as s2
+from questions.Geography_question import geography_question as s3
 
 def play_quiz(pass_input):
     if pass_input == '1':
         s = s1()
     elif pass_input == '2':
         s = s2()
+    elif pass_input == '3':
+        s = s3()
         
     random.shuffle(s)
     score = 0
